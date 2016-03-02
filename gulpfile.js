f561eae71eade11308866b1f1536d8934f4fb7b3
peterpetrekanics <peter.petrekanics@liferay.com>
2016-03-02 11:50:21 +0100

    march2 training stuff added

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,16 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var bourbon = require('node-bourbon'); 
 
 gulp.task('sass', function () {
-  return gulp.src('./src/scss/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
+  return gulp.src('./bower_components/lexicon/release/scss/*.scss')
+    .pipe(
+    	sass(
+    		{
+				includePaths: bourbon.includePaths
+			}
+		).on('error', sass.logError)
+	)
     .pipe(gulp.dest('./build/css'));
 });
 
